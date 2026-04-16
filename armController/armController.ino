12139a1efd9a7abbe149ab36800fd26ce48b92b6
Amogh Shetty <amoghshetty.sj@gmail.com>
2026-04-16 23:01:25 +0530

    Refactor servo control logic in armController.ino
    Refactor armController.ino to manage servo angles based on input values. Removed unused variables and implemented angle conversion for servo control.

--- armController/armController.ino
+++ armController/armController.ino
@@ -1,18 +1,35 @@
 #include <string.h>
+#include <Servo.h>
 
-float values[5];      // Float array for your data
-float prev_values[] = {0,0,0,0,0};
-char buffer[100];      // Serial buffer
+const int servo_nos = 4;
+const int servoPins[servo_nos] = {5, 6, 10, 11};
+
+Servo servos[servo_nos];
+
+float values[5];      
+char buffer[100];      
 int valueCount = 0;
+
+int angles[5] = {0,0,0,0,0};
 
 void setup() {
   Serial.begin(9600);
+
+  for (int i = 0; i < servo_nos; i++) {
+    servos[i].attach(servoPins[i]);
+    servos[i].write(90);   // start position
+  }
 }
 
 void loop() {
   if (Serial.available()) {
+
+    //reading input values
+
     int len = Serial.readBytesUntil('!', buffer, sizeof(buffer)-1);
     buffer[len] = '\0'; 
+
+    // putting values in values array
 
     char *ptr = strtok(buffer, ",");
     valueCount = 0;
@@ -22,24 +39,20 @@ void loop() {
       valueCount++;
       ptr = strtok(NULL, ",");
     }
-    Serial.println(valueCount);
+
+    // main logic if inputted values are of length 5
+
     if(valueCount == 5){
       for (int i = 0; i < valueCount; i++) {
-        Serial.println(values[1]);
-        Serial.println(prev_values[1]);
-        if(values[1] < prev_values[1]){
-          //finger closing code 
-          Serial.println("Finger Closing ");
-        }
-        else if(values[1] > prev_values[1]){
-          //finger opening code
-          Serial.println("Finger Opening");
-        }
-
-        prev_values[i] = values[i];
+        angles[i] = int_to_deg(values[i]);
+      }
+      for (int i = 0; i < valueCount; i++){
+        servos[i].write(angles[i]);
       }
     }
-
-    
   }
+}
+
+int int_to_deg(float a){
+  return ((int)(a * 90.0));
 }
